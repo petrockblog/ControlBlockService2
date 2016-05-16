765200f33851a3bf42ec5b637beecdcd685b44b4
petrockblog <contact@petrockblock.com>
2016-05-16 10:12:08 +0200

    AUpdated travis scripts

--- test/travis-ci.sh
+++ test/travis-ci.sh
@@ -17,15 +17,15 @@ TEST_COMMAND="make"
 
 function setup_arm_chroot {
     # Host dependencies
-    sudo apt-get install -qq -y ${HOST_DEPENDENCIES}
+    apt-get install -qq -y ${HOST_DEPENDENCIES}
 
     # Create chrooted environment
-    sudo mkdir ${CHROOT_DIR}
-    sudo debootstrap --foreign --no-check-gpg --include=fakeroot,build-essential \
+    mkdir ${CHROOT_DIR}
+    debootstrap --foreign --no-check-gpg --include=fakeroot,build-essential \
         --arch=${CHROOT_ARCH} ${VERSION} ${CHROOT_DIR} ${MIRROR}
-    sudo cp /usr/bin/qemu-arm-static ${CHROOT_DIR}/usr/bin/
-    sudo chroot ${CHROOT_DIR} ./debootstrap/debootstrap --second-stage
-    sudo sbuild-createchroot --arch=${CHROOT_ARCH} --foreign --setup-only \
+    cp /usr/bin/qemu-arm-static ${CHROOT_DIR}/usr/bin/
+    chroot ${CHROOT_DIR} ./debootstrap/debootstrap --second-stage
+    sbuild-createchroot --arch=${CHROOT_ARCH} --foreign --setup-only \
         ${VERSION} ${CHROOT_DIR} ${MIRROR}
 
     # Create file with environment variables which will be used inside chrooted
@@ -35,19 +35,19 @@ function setup_arm_chroot {
     chmod a+x envvars.sh
 
     # Install dependencies inside chroot
-    sudo chroot ${CHROOT_DIR} apt-get update
-    sudo chroot ${CHROOT_DIR} apt-get --allow-unauthenticated install \
+    chroot ${CHROOT_DIR} apt-get update
+    chroot ${CHROOT_DIR} apt-get --allow-unauthenticated install \
         -qq -y ${GUEST_DEPENDENCIES}
 
     # Create build dir and copy travis build files to our chroot environment
-    sudo mkdir -p ${CHROOT_DIR}/${TRAVIS_BUILD_DIR}
-    sudo rsync -av ${TRAVIS_BUILD_DIR}/ ${CHROOT_DIR}/${TRAVIS_BUILD_DIR}/
+    mkdir -p ${CHROOT_DIR}/${TRAVIS_BUILD_DIR}
+    rsync -av ${TRAVIS_BUILD_DIR}/ ${CHROOT_DIR}/${TRAVIS_BUILD_DIR}/
 
     # Indicate chroot environment has been set up
-    sudo touch ${CHROOT_DIR}/.chroot_is_done
+    touch ${CHROOT_DIR}/.chroot_is_done
 
     # Call ourselves again which will cause tests to run
-    sudo chroot ${CHROOT_DIR} bash -c "cd ${TRAVIS_BUILD_DIR} && ./.travis-ci.sh"
+    chroot ${CHROOT_DIR} bash -c "cd ${TRAVIS_BUILD_DIR} && ./.travis-ci.sh"
 }
 
 if [ -e "/.chroot_is_done" ]; then
