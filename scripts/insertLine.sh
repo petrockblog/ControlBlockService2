81c1b84b7397caca533c07fefec507731e635b8a
Florian <info@petrockblock.com>
2017-01-07 21:42:25 +0100

    Added service installation targets

--- scripts/insertLine.sh
+++ scripts/insertLine.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-echo -e "Making sure that spi-dev is contained in /etc/modules ..."
-if [[ -z $(cat /boot/config.txt | grep "dtparam=spi=on") ]]; then
-	sed -i '$a dtparam=spi=on' /boot/config.txt
-fi
-modprobe spi_bcm2835
+
+function addSPIBcmModule() {
+    echo -e "Making sure that spi-dev is contained in /etc/modules ..."
+    if [[ -z $(cat /boot/config.txt | grep "dtparam=spi=on") ]]; then
+        sed -i '$a dtparam=spi=on' /boot/config.txt
+    fi
+    modprobe spi_bcm2835
+}
+
+touch /etc/modprobe.d/raspi-blacklist.conf
+sed -i -e "s|^blacklist spi-bcm2708|#blacklist spi-bcm2708|g" /etc/modprobe.d/raspi-blacklist.conf
+addSPIBcmModule
+install -m 0755 controlblockservice /etc/init.d
+update-rc.d controlblockservice defaults
+/etc/init.d/controlblockservice start
