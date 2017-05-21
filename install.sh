1fc9913433a617ac62ae095f3700af6883874abf
petrockblog <contact@petrockblock.com>
2017-05-21 10:19:11 +0200

    Updated installation scripts

--- install.sh
+++ install.sh
@@ -1,20 +1,31 @@
-!#/usr/bin 
+#!/bin/bash
+
+# check, if sudo is used
+if [[ "$(id -u)" -ne 0 ]]; then
+    echo "Script must be run under sudo from the user you want to install for. Try 'sudo $0'"
+    exit 1
+fi
 
 # ensure that all needed OS packages are installed
 apt-get install -y git cmake g++-4.9 doxygen || (c=$?; echo "Error during installation of APT packages"; (exit $c))
 
-# download the driver sources from the repository to the local machine
-git clone --recursive https://github.com/petrockblog/ControlBlockService2.git || (c=$?; echo "Error while downloading the repository"; (exit $c))
+# make sure that the submodule data is available
+git submodule update --init --recursive
 
-# change into the driver directory
-pushd ControlBlockService2 || (c=$?; echo "Error while changing into the folder ControlBlockService2"; (exit $c))
+if [[ -d build ]]; then
+    rm -rf build
+fi
 
 # create a folder that will contain all build artefacts and change into that folder
 mkdir build || (c=$?; echo "Error while creating build folder"; (exit $c))
-cd build || (c=$?; echo "Error while changing into the folder build"; (exit $c))
+pushd build || (c=$?; echo "Error while changing into the folder build"; (exit $c))
 
 # create Makefiles
 cmake .. || (c=$?; echo "Error while generating Makefiles"; (exit $c))
+
+# ensure that no old instance of the driver is running and installed
+make uninstallservice
+make uninstall
 
 # build driver binary
 make || (c=$?; echo "Error during building binary"; (exit $c))
@@ -27,3 +38,18 @@ make installservice || (c=$?; echo "Error during installation of service"; (exit
 
 # change back into the original folder
 popd
+
+# sanity checks
+# check that the binary is installed
+if [[ ! -f /usr/bin/controlblock ]]; then
+    echo "[ERROR] The ControlBlock driver binary is not installed"
+else
+    echo "[SUCCESS] The ControlBlock driver binary is installed"
+fi
+
+# check that the service is running
+ps -ef | grep controlblockservice | grep -v grep
+[ $?  -eq "0" ] && echo "[SUCCESS] The ControlBlock service is running" || echo "[ERROR] The ControlBlock service is not running"
+
+echo "You can find the configuration file at /etc/controlblockconfig.cfg".
+
--- uninstall.sh
+++ uninstall.sh
@@ -1,33 +1,33 @@
-!#/usr/bin 
+#!/bin/bash
+
+# check, if sudo is used
+if [[ "$(id -u)" -ne 0 ]]; then
+    echo "Script must be run under sudo from the user you want to install for. Try 'sudo $0'"
+    exit 1
+fi
 
 # ensure that all needed OS packages are installed
 apt-get install -y git cmake g++-4.9 doxygen || (c=$?; echo "Error during installation of APT packages"; (exit $c))
 
-if [[ ! -d ControlBlockService2 ]]; then
-	# download the driver sources from the repository to the local machine
-	git clone --recursive https://github.com/petrockblog/ControlBlockService2.git || (c=$?; echo "Error while downloading the repository"; (exit $c))
-	# change into the driver directory
-	pushd ControlBlockService2 || (c=$?; echo "Error while changing into the folder ControlBlockService2"; (exit $c))
-else
-	# change into the driver directory
-	pushd ControlBlockService2 || (c=$?; echo "Error while changing into the folder ControlBlockService2"; (exit $c))
-	# update sources to latest version
-	git pull
-fi
+# make sure that the submodule data is available
+git submodule update --init --recursive
 
+if [[ -d build ]]; then
+    rm -rf build
+fi
 
 # create a folder that will contain all build artefacts and change into that folder
 mkdir build || (c=$?; echo "Error while creating build folder"; (exit $c))
-cd build || (c=$?; echo "Error while changing into the folder build"; (exit $c))
+pushd build || (c=$?; echo "Error while changing into the folder build"; (exit $c))
 
 # create Makefiles
 cmake .. || (c=$?; echo "Error while generating Makefiles"; (exit $c))
 
-# install the binary
-make uninstall || (c=$?; echo "Error during uninstalling binary"; (exit $c))
-
-# install the driver as a service
-make uninstallservice || (c=$?; echo "Error during uninstalling service"; (exit $c))
-
-# change back into the original folder
+# ensure that no old instance of the driver is running and installed
+make uninstallservice
 popd
+
+# check that the service is not running anymore
+ps -ef | grep controlblockservice | grep -v grep
+[ $?  -eq "0" ] && echo "[ERROR] Could not remove the ControlBlock service" || echo "[SUCCESS] The ControlBlock service was successfully removed."
+
