5bcae537cfbebc2ee1712b485da059b918be9701
petrockblog <info@petrockblock.com>
2019-09-04 22:13:35 +0200

    Removed unnecessary device-tree overlay

--- scripts/insertLine.sh
+++ scripts/insertLine.sh
@@ -13,15 +13,7 @@ touch /etc/modprobe.d/raspi-blacklist.conf
 sed -i -e "s|^blacklist spi-bcm2708|#blacklist spi-bcm2708|g" /etc/modprobe.d/raspi-blacklist.conf
 addSPIBcmModule
 
-# install GPIO device tree overlay
-apt-get install -q -y device-tree-compiler
-dtc -@ -I dts -O dtb -o gpio_pull-overlay.dtb gpio_pull-overlay.dts
-cp gpio_pull-overlay.dtb /boot/overlays
-grep -qxF 'dtoverlay=gpio_pull' /boot/config.txt || echo 'dtoverlay=gpio_pull' >> /boot/config.txt
-
 # install ControlBlockService files
 install -m 0755 controlblockservice /etc/init.d
 update-rc.d controlblockservice defaults
 /etc/init.d/controlblockservice start
-
-echo "!!! Please note that YOU NEED TO REBOOT to let the driver be effective. !!!"
